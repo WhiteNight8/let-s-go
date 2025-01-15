9cdba9e8f8ae562c232320d82f1dfb50e02fc18f
Xu <whitenight.xiaxu@gmail.com>
2025-01-15 21:16:51 +0800

    deliberate error

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -21,33 +21,30 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		app.errorLog.Print(err.Error())
-		http.Error(w, "internal server error", 500)
+		app.serverError(w, err)
 		return
 	}
 
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		app.errorLog.Print(err.Error())
-		http.Error(w, "internal server error", 500)
+		app.serverError(w, err)
 	}
-
-	w.Write([]byte("hello from xu"))
 }
 
 func (app *application) homeView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 
-	fmt.Fprintf(w, "display  home page with id", id)
+	fmt.Fprintf(w, "display  home page withid  %id", id)
 }
 
 func (app *application) homeCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Mehtod not allowed", http.StatusMethodNotAllowed)
+		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
 
